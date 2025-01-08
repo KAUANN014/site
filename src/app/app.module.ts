66c85772c06e4ee884f65086ce750ca1e52979a1
Kauan de Oliveira Carvalho <kauan.carvalho@corregedoria.df>
2025-01-08 11:42:50 -0300

    testando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BotaoComponent } from './botao/botao.component';
+import { HeaderComponent } from './header/header.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotaoComponent,
+    HeaderComponent,
+    FlexboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/flexbox/flexbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flexbox',
+  standalone: false,
+  
+  templateUrl: './flexbox.component.html',
+  styleUrl: './flexbox.component.scss'
+})
+export class FlexboxComponent {
+
+}
