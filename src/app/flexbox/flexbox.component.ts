ea6edb1095b2f6e7d60f88368d9d8ee1f1eedd72
Kauan de Oliveira Carvalho <kauan.carvalho@corregedoria.df>
2025-01-09 11:14:04 -0300

    Teste

--- src/app/flexbox/flexbox.component.ts
+++ src/app/flexbox/flexbox.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-flexbox',
   standalone: false,
-  
+
   templateUrl: './flexbox.component.html',
   styleUrl: './flexbox.component.scss'
 })
